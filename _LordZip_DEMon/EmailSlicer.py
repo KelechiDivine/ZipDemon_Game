ad28d87eab7639bb2cb04f944d757b9bee0dc049
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-08-02 10:06:51 +0100

    email slicer completed

---
+++ _LordZip_DEMon/EmailSlicer.py
@@ -0,0 +1,15 @@
+# email= input("Enter your email: ").strip()
+# user_name= email[:email.index('@')]
+# domain= email[email.index('@') + 1]
+#
+# name_placeholder = 'Your user name is {}'.format(user_name) + ' & domain is ' + '{}'.format(domain)
+# print(name_placeholder)
+
+
+"""A more pythonical way of doing this is by using a function"""
+
+def email_slicer(emails):
+    username, _, domain= emails.strip().partition("@")
+    return 'Your user name is {}'.format(username) + ' & domain is ' + '{}'.format(domain)
+user_input= input("Enter your name here: ")
+print(email_slicer(user_input))
